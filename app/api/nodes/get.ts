65125d3d0c0a531f1f80e5460184ecfc38bc4355
Uisneach <grandsonoffinn@protonmail.com>
2026-01-22 21:09:25 -0500

    Move api folder for better vercel detection.

--- app/api/nodes/get.ts
+++ app/api/nodes/get.ts
@@ -1,5 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
-import { runQuery } from '../../lib/neo4j';
+import { runQuery } from '@/lib/neo4j';
 
 export default async function handler(
   req: NextApiRequest,
