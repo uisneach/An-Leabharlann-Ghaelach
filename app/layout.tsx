9857983c7d580cfd71bc9b208bdde8e37fdabfd2
Uisneach <grandsonoffinn@protonmail.com>
2026-03-08 23:37:51 -0400

    Close meta tags.

--- app/layout.tsx
+++ app/layout.tsx
@@ -43,8 +43,7 @@ export default function RootLayout({
   return (
     <html lang="en">
     <head>
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <meta charset="UTF-8">
+      <meta charSet="UTF-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <title>An Leabharlann Ghaelach</title>
       <link rel="icon" type="image/x-icon" href="https://uisneac.com/assets/Gold-Celtic-Design.png" />
